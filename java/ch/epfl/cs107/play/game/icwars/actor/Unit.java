bac8fad2dd28bf86963122e577d13aa171c097cc
Henri Antal <henrimaxim@gmail.com>
2021-12-06 16:02:20 +0100

    Unit.java should be finished not sure if the get methods work and also what they are used for.

--- java/ch/epfl/cs107/play/game/icwars/actor/Unit.java
+++ java/ch/epfl/cs107/play/game/icwars/actor/Unit.java
@@ -2,34 +2,100 @@ package ch.epfl.cs107.play.game.icwars.actor;
 
 import ch.epfl.cs107.play.game.areagame.Area;
 import ch.epfl.cs107.play.game.areagame.actor.Orientation;
+import ch.epfl.cs107.play.game.areagame.actor.Sprite;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.math.Vector;
 
 public class Unit extends ICWarsActor{
 
-
     String name;
+    int Hp;
+    private Sprite sprite;
+    int attackDamage;
+    int maxRadius;
 
-    Unit(String name, Area owner, Orientation orientation, DiscreteCoordinates coordinates, String belongs ){
+    public Unit(String name, Area owner, Orientation orientation, DiscreteCoordinates coordinates, String belongs ){
         super(owner, orientation, coordinates, belongs);
 
-        int Hp;
-        int damage;
+        this.name = name;
+        // somehow need to include damage taken (- Hp)
+        // also need to include healing (+ Hp)
+
 
 
     }
+    public String getName() {
+        return name;
+    }
+
+    public int getHp() {
+        return Hp;
+    }
+
+    public int getDamage(){
+        return attackDamage;
+    }
+
 
     public class Soldier extends Unit{
 
         Soldier(String name, Area owner, Orientation orientation, DiscreteCoordinates coordinates, String belongs) {
             super(name, owner, orientation, coordinates, belongs);
+            super.maxRadius = 2;
+            super.attackDamage = 2;
+            super.Hp = 5;
+            if (belongs.equals("ally")) {
+                Sprite sprite = new Sprite("icwars / friendlySoldier" , 1.f, 1.f, this, null, new Vector(-0.25f, -0.25f));
+            } else {
+                Sprite sprite = new Sprite("icwars / enemySoldier" , 1.f, 1.f, this, null, new Vector(-0.25f, -0.25f));
+            }
+
+
+        }
+
+        // TODO not sure if these 2 methods below work.
+        @Override
+        public int getHp() {
+            return super.getHp();
+        }
+
+        @Override
+        public int getDamage() {
+            return super.getDamage();
+        }
+
+    }
+
+    public class Tank extends Unit{
+
+        Tank(String name, Area owner, Orientation orientation, DiscreteCoordinates coordinates, String belongs) {
+            super(name, owner, orientation, coordinates, belongs);
+            super.maxRadius = 4;
+            super.attackDamage = 7;
+            super.Hp = 10;
+            if (belongs.equals("ally")) {
+                Sprite sprite = new Sprite("icwars / friendlyTank" , 1.f, 1.f, this, null, new Vector(-0.25f, -0.25f));
+            } else {
+                Sprite sprite = new Sprite("icwars / enemyTank" , 1.f, 1.f, this, null, new Vector(-0.25f, -0.25f));
+            }
+
+        }
+
+        // TODO not sure if these 2 methods below work.
+        @Override
+        public int getHp() {
+            return super.getHp();
+        }
+
+        @Override
+        public int getDamage() {
+            return super.getDamage();
         }
 
     }
 
 
-    public String getName() {
-        return name;
-    }
+
 
 
 
